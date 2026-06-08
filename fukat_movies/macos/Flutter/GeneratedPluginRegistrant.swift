eecd5d9fe4b20d79bad28b35baf2f00893674a1e
venxm14 <gawassuraj090@gmail.com>
2026-06-09 01:50:04 +0530

    replace fromm media player video_player and chewie too  media_kit

--- fukat_movies/macos/Flutter/GeneratedPluginRegistrant.swift
+++ fukat_movies/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,22 +7,24 @@ import Foundation
 
 import app_links
 import flutter_inappwebview_macos
+import media_kit_libs_macos_video
+import media_kit_video
 import mobile_scanner
 import package_info_plus
 import shared_preferences_foundation
 import sqflite_darwin
 import url_launcher_macos
-import video_player_avfoundation
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  MediaKitLibsMacosVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitLibsMacosVideoPlugin"))
+  MediaKitVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitVideoPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  VideoPlayerPlugin.register(with: registry.registrar(forPlugin: "VideoPlayerPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
--- fukat_movies/windows/flutter/generated_plugins.cmake
+++ fukat_movies/windows/flutter/generated_plugins.cmake
@@ -5,6 +5,8 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   app_links
   flutter_inappwebview_windows
+  media_kit_libs_windows_video
+  media_kit_video
   url_launcher_windows
 )
 
